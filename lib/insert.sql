dd1723cc6b9774e8b12ced9a51cfcaf1530a8448
Daniel Long <danielwlong618@gmail.com>
2024-03-05 22:30:39 -0600

    lab done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'white', 'good', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'white', 'good', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'M', 'white', 'good', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'white', 'good', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'M', 'white', 'good', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'white', 'good', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'brown', 'good', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'F', 'black', 'good', 0);
